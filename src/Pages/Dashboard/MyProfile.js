4d4e7d5588d326df7f848636444b65a0fa76cc4d
rohulaminwd <rohulaminwd@gmail.com>
2022-05-24 08:59:44 +0600

    portfilo section add

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -8,18 +8,18 @@ const MyProfile = () => {
     return (
         <div>
             <h1 className='text-2xl font-bold text-primary text-center'>My Profile</h1>
-            <div class="card w-[100%] mt-16 mx-auto lg:w-[500px] bg-base-100 shadow-xl">
+            <div class="card w-[100%] mt-16 mx-auto lg:w-[500px] bg-base-100 border-8 border border-blue-200 rounded-2xl shadow-xl">
                 <div className="avatar mt-5">
-                <div class="w-28 mx-auto rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                    <img src={user.photoURL} />
-                </div>
+                    <div class="w-28 mx-auto rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={user.photoURL} />
+                    </div>
                 </div>
                 <div class="card-body items-center text-center">
                     <h2 class="text-3xl text-primary font-bold">{user.displayName}</h2>
                     <h2 class="text-xl text-primary font-bold">{user.email}</h2>
                     <p>I am a student of class inter 2nd Year Hazi Nurul Haque Nonni poragong Mointri collage</p>
-                    <a href="tel:+0183194559" ><button className='btn btn-primary font-bold btn-sm'><AiFillPhone /> +0183194559 </button></a>
                     <div class="card-actions">
+                    <a href="tel:+0183194559" ><button className='btn btn-primary font-bold btn-sm'><AiFillPhone /> +0183194559 </button></a>
                     <button class="btn btn-sm btn-secondary">Update Profile</button>
                     </div>
                 </div>
--- src/Pages/Protfolio/Protfolio.js
+++ src/Pages/Protfolio/Protfolio.js
@@ -1,9 +1,106 @@
 import React from 'react';
+import rohul from '../../assets/images/rohul.png'
+import {SiPolymerproject} from 'react-icons/si'
+import {AiFillPhone} from 'react-icons/ai'
 
 const Protfolio = () => {
     return (
-        <div className='mx-4 md:text-8xl lg:text-16'>
-            <h1 className='text-2xl font-bold text-center text-cyan-500 my-8'>My Portfolio </h1>
+        <div className='mt-20 mx-4 md:mx-8 lg:mx-16'>
+            <h1 className='text-3xl uppercase font-bold text-center text-cyan-500 my-8'>My Portfolio</h1>
+            <div className="border text-center md:w-[500px] mx-auto p-5 border-x-8 border-cyan-200 rounded-2xl shadow-xl">
+                <div class="avatar">
+                    <div class="w-32 mask mask-squircle">
+                        <img src={rohul} />
+                    </div>
+                </div>
+                <div className="">
+                    <h1 className='text-cyan-500 font-bold text-2xl'>Rohul amin</h1>
+                    <h1 className='font-bold'>Bathkuchi, Nalitabari, Sherpur</h1>
+                    <p className=''>I am a student of class Inter 2nd Year Hazi Nururl Haquea Nonni puragong Mointri Collage. and I am junior web developer of MERN stack</p>
+                </div>
+            </div>
+            <h1 className='text-3xl uppercase font-bold text-center underline underline-offset-4 text-blue-500 my-10'>Others Information</h1>
+            <div className="grid md:grid-cols-1 mt-10 lg:grid-cols-2 gap-5">
+                <div className="border p-3 border-x-8 border-blue-200 rounded-2xl shadow-xl">
+                    <h1 className='text-primary mb-5 font-bold text-2xl'>Coding Skills</h1>
+                    <div className="grid gap-2">
+                        <div className="">
+                            <h1 className='text-blue-500 mb-0 font-bold'>HTML & CSS: <span className='text-primary'>90%</span></h1>
+                            <progress class="progress progress-primary mt-0 h-[16px] w-full" value="90" max="100"></progress>
+                        </div>
+                        <div className="">
+                            <h1 className='text-blue-500 mb-0 font-bold'>JavaScript : <span className='text-primary'>60%</span></h1>
+                            <progress class="progress progress-primary mt-0 h-[16px] w-full" value="60" max="100"></progress>
+                        </div>
+                        <div className="">
+                            <h1 className='text-blue-500 mb-0 font-bold'>Node js: <span className='text-primary'>40%</span></h1>
+                            <progress class="progress progress-primary mt-0 h-[16px] w-full" value="40" max="100"></progress>
+                        </div>
+                        <div className="">
+                            <h1 className='text-blue-500 mb-0 font-bold'>React JS: <span className='text-primary'>80%</span></h1>
+                            <progress class="progress progress-primary mt-0 h-[16px] w-full" value="80" max="100"></progress>
+                        </div>
+                        <div className="">
+                            <h1 className='text-blue-500 mb-0 font-bold'>WordPress: <span className='text-primary'>92%</span></h1>
+                            <progress class="progress progress-primary mt-0 h-[16px] w-full" value="92" max="100"></progress>
+                        </div>
+                    </div>
+                </div>
+                <div className="border p-3 border-x-8 border-blue-200 rounded-2xl shadow-xl">
+                    <h1 className='text-primary mb-5 font-bold text-2xl'>Educational Skills</h1>
+                    <div className="grid gap-2">
+                        <div className="">
+                            <h1 className='text-secondary text-xl mb-0 font-bold'>HSC - 2020</h1>
+                            <h1 className='text-blue-500 mb-0 font-bold'>Haji Nurul Haqua Nonni Puragong Moitri Collage</h1>
+                        </div>
+                        <div className="">
+                            <h1 className='text-secondary text-xl mb-0 font-bold'>SSC - 2018</h1>
+                            <h1 className='text-blue-500 mb-0 font-bold'>Polasi kura Jonota High School</h1>
+                        </div>
+                        <h1 className='text-primary mb-5 font-bold text-2xl'>My best Project</h1>
+                        <div className="grid grid-cols-3 gap-2">
+                            <a href="https://grocery-stock-90a1c.web.app/" className='border p-3 border-x-8 border-blue-200 rounded-full'>
+                                <h1 className='text-blue-500 mb-0 font-bold flex items-center'><SiPolymerproject width={24} />  Red Onion</h1>
+                            </a>
+                            <a href="https://grocery-stock-90a1c.web.app/" className='border p-3 border-x-8 border-blue-200 rounded-full'>
+                                <h1 className='text-blue-500 mb-0 font-bold flex items-center'><SiPolymerproject width={24} />  Red Onion</h1>
+                            </a>
+                            <a href="https://grocery-stock-90a1c.web.app/" className='border p-3 border-x-8 border-blue-200 rounded-full'>
+                                <h1 className='text-blue-500 mb-0 font-bold flex items-center'><SiPolymerproject width={24} />  Red Onion</h1>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h1 className='text-3xl uppercase font-bold text-center underline underline-offset-4 text-blue-500 my-10'>contact Information</h1>
+            <div className="border p-3 border-x-8 my-10 border-blue-200 rounded-2xl shadow-xl">
+                    <h1 className='text-primary mb-5 font-bold text-2xl'>Contact Information</h1>
+                    <div className="grid gap-2">
+                        <div className="">
+                            <h1 className='text-secondary text-xl mb-0 font-bold'>Address</h1>
+                            <h1 className='text-blue-500 mb-0 font-bold'>Batkuchi, Nonni, Nalitabari, Serpur.</h1>
+                        </div>
+                        <div className="">
+                            <h1 className='text-secondary text-xl mb-0 font-bold'>Phone Number</h1>
+                            <div class="card-actions mt-3">
+                                <a href="tel:+0183194559" ><button className='btn btn-primary font-bold btn-sm'><AiFillPhone /> +0183194559 </button></a>
+                                <a href="tel:+0183194559" ><button className='btn btn-primary font-bold btn-sm'><AiFillPhone /> +0183194559 </button></a>
+                            </div>
+                        </div>
+                        <h1 className='text-primary mb-5 font-bold text-2xl'>My best Project</h1>
+                        <div className="grid grid-cols-3 gap-2">
+                            <a href="https://grocery-stock-90a1c.web.app/" className='border p-3 border-x-8 border-blue-200 rounded-full'>
+                                <h1 className='text-blue-500 mb-0 font-bold flex items-center'><SiPolymerproject width={24} />  Red Onion</h1>
+                            </a>
+                            <a href="https://grocery-stock-90a1c.web.app/" className='border p-3 border-x-8 border-blue-200 rounded-full'>
+                                <h1 className='text-blue-500 mb-0 font-bold flex items-center'><SiPolymerproject width={24} />  Red Onion</h1>
+                            </a>
+                            <a href="https://grocery-stock-90a1c.web.app/" className='border p-3 border-x-8 border-blue-200 rounded-full'>
+                                <h1 className='text-blue-500 mb-0 font-bold flex items-center'><SiPolymerproject width={24} />  Red Onion</h1>
+                            </a>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 };
